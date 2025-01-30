c2061063fcb929b48bc5608aa79da5c47fd87b6f
vsiletskyi <vsiletskyi@gmail.com>
2025-01-30 21:49:20 +0200

    header fixed 2

--- src/components/HeaderMenu/HeaderMenu.jsx
+++ src/components/HeaderMenu/HeaderMenu.jsx
@@ -1,19 +1,19 @@
 import styles from './HeaderMenu.module.css'
 
 const menuList = [
-    { label: 'Home', href: '/' },
-    { label: 'Features', href: '/features' },
-    { label: 'Pricing', href: '/pricing' },
-    { label: 'About us', href: '/about' },
-    { label: 'Contact', href: '/contact' }
+    { label: 'Home', href: '#home' },
+    { label: 'Features', href: '#features' },
+    { label: 'Pricing', href: '#pricing' },
+    { label: 'About us', href: '#about' },
+    { label: 'Contact', href: '#contact' }
 ]
 
 const HeaderMenu = () => {
   return (
     <nav>
-        <ul className={styles.header__menu}>
+        <ul className={styles.menu}>
               {menuList.map((item, i) => {
-                return <li key={i} className={styles.menu__item}><a href={item.href}>{item.label}</a></li>
+                return <li key={i} className={styles.item}><a href={item.href}>{item.label}</a></li>
             })}  
         </ul>
     </nav>
